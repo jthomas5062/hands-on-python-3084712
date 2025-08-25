e6026698c61de3d1a50e0e4edc3a306105d8f9aa
James Thomas <jthomas5062@gmail.com>
2025-08-25 17:47:53 +0000

    python hangman game + wordslist

---
+++ JT_practice/wordslist.py
@@ -0,0 +1,5 @@
+# words for hangman game
+
+words = ("apple", "orange", "banana", "coconut", "pineapple",
+         "foo", "bar", "baz", "qux", "quux", "corge", "grault",
+         "garply", "waldo", "fred", "plugh", "xyzzy", "thud")
